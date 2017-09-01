42cc0e0ac7879267d8282a61ee7560cfa8927be7
yodnorobov <yodnorobov@team.amocrm.com>
2017-09-01 15:39:41 +0300

    Update source.js

--- source.js
+++ source.js
@@ -1,4 +1,5 @@
 define(['jquery', 'lib/components/base/modal'], function ($, Modal) {
+
   var Contact = {};
 
   Contact.data = [];
@@ -6,17 +7,16 @@ define(['jquery', 'lib/components/base/modal'], function ($, Modal) {
 
   Contact.render = function (self) {
 
-    console.log('render function');
-    console.log(self.system().area);
+    //console.log(self.system().area);
 
     if (self.system().area === 'cucard' || 'lcard') {
       console.log(self.system().area);
     }
 
-    var lang = self.i18n('userLang');
-    w_code = self.get_settings().widget_code; //в данном случае w_code='new-widget'
-
-    console.log(w_code);
+    //var lang = self.i18n('userLang');
+    //w_code = self.get_settings().widget_code; //в данном случае w_code='new-widget'
+    //
+    //console.log(w_code);
 
     self.render_template({
       caption: {
@@ -29,6 +29,45 @@ define(['jquery', 'lib/components/base/modal'], function ($, Modal) {
     });
 
     return true;
+  };
+
+  Contact.bind_function = function (self) {
+    if (self.system().area === 'culist' || 'llist') {
+
+      $('.search_field_text').keyup(function () {
+        var value = $('.search_field_text').val();
+
+        //console.log(value);
+
+        $.ajax({
+          type: "GET",
+          url: 'https://amobase.amocrm.ru/article/search?q=' + value + '&page=1&',
+          success: function (data) {
+            console.log(
+              Contact.display_modal()
+            )
+          }
+          //dataType: 'json'
+        });
+      });
+
+      //$('.ac-form-button').on('click', function () {
+      //  var search_data = $('.search_field_text').text();
+      //  console.log('BUTTON CLICKED' + search_data);
+      //});
+    }
+    return true;
+  };
+
+  Contact.display_modal = function () {
+    //var string = '';
+    //for (var i = 0; i < contacts.length; i++) {
+    //  string = string + contacts[i];
+    //}
+    //if (contacts[0].length !== 0) {
+    var modal = Contact.modal("есть дубли!");
+    setTimeout(modal.destroy.bind(modal), 5000);
+    //}
   };
 
   Contact.modal = function (data) {
@@ -46,44 +85,6 @@ define(['jquery', 'lib/components/base/modal'], function ($, Modal) {
       }
     });
     return modal;
-  };
-
-  Contact.bind_function = function (self) {
-    if (self.system().area === 'culist' || 'llist') {
-
-      $('.search_field_text').keyup(function(){
-        var value = $('.search_field_text').val();
-
-        console.log(value);
-
-        $.ajax({
-          type: "GET",
-          url: 'https://amobase.amocrm.ru/article/search?q=' + value + '&page=1&',
-          success: function(data){console.log(data)}
-          //dataType: 'json'
-        });
-
-        //$('#errmsg').empty();
-        //$('#errmsg').text(Value);
-      });
-
-      //$('.ac-form-button').on('click', function () {
-      //  var search_data = $('.search_field_text').text();
-      //  console.log('BUTTON CLICKED' + search_data);
-      //});
-    }
-    return true;
-  };
-
-  Contact.display_modal = function (contacts) {
-    var string = '';
-    for (var i = 0; i < contacts.length; i++) {
-      string = string + contacts[i];
-    }
-    if (contacts[0].length !== 0) {
-      var modal = Contact.modal("есть дубли! " + '<br>' + string);
-      setTimeout(modal.destroy.bind(modal), 5000);
-    }
   };
 
   Contact.init = function (self) {
