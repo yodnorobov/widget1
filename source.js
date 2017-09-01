e39a0002e75843859824121d109a0e0842862ff9
yodnorobov <yodnorobov@team.amocrm.com>
2017-09-01 14:31:27 +0300

    Update source.js

--- source.js
+++ source.js
@@ -30,9 +30,6 @@ define(['jquery', 'lib/components/base/modal'], function ($, Modal) {
               </div>\
                   <div id="js-ac-sub-subs-container">\
                   </div>\
-
-                  <input class="search_field_text" name="q" type="text">\
-
                   <div class="ac-form-button ac_sub">SEND</div>\
                   </div>\
               <div class="ac-already-subs"></div>\
