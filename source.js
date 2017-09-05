7b3b80774bc2624b790efc196471d2d3ad79fd3b
yodnorobov <yodnorobov@team.amocrm.com>
2017-09-05 10:39:25 +0300

    Update source.js

--- source.js
+++ source.js
@@ -24,22 +24,21 @@ define(['jquery', 'lib/components/base/modal'], function ($, Modal) {
   Contact.bind_function = function (self) {
     if (self.system().area === 'cucard' || 'lcard') {
       var globalTimeout = null;
-      
+
       $('.search_field_text').keyup(function () {
-        
-         if (globalTimeout != null) {
-    clearTimeout(globalTimeout);
-  }
-  globalTimeout = setTimeout(function() {
-    globalTimeout = null;  
-        var value = $('.search_field_text').val();
-        console.log(value);
-        var url = 'https://' + AMOCRM.widgets.system.subdomain + '.amocrm.ru/widgets/' + AMOCRM.widgets.system.subdomain + '/loader/code_47/proxy2/?link=https://amobase.amocrm.ru/search?q=' + value + '&page=1&amouser=' + AMOCRM.widgets.system.amouser + '&amohash=' + AMOCRM.widgets.system.amohash;
-        var url2 = 'https://test1-yodnorobov.codeanyapp.com/proxy.php';
-    
-    
-    Contact.modal(value);
-    
+
+        if (globalTimeout != null) {
+          clearTimeout(globalTimeout);
+        }
+        globalTimeout = setTimeout(function () {
+          globalTimeout = null;
+          var value = $('.search_field_text').val();
+          console.log(value);
+          //var url = 'https://' + AMOCRM.widgets.system.subdomain + '.amocrm.ru/widgets/' + AMOCRM.widgets.system.subdomain + '/loader/code_47/proxy2/?link=https://amobase.amocrm.ru/search?q=' + value + '&page=1&amouser=' + AMOCRM.widgets.system.amouser + '&amohash=' + AMOCRM.widgets.system.amohash;
+          //var url2 = 'https://test1-yodnorobov.codeanyapp.com/proxy.php';
+          
+          Contact.modal(value);
+
 //     $.ajax({
 //           type: "GET",
 //           url: url,
@@ -48,8 +47,8 @@ define(['jquery', 'lib/components/base/modal'], function ($, Modal) {
 //             Contact.display_modal(data);
 //           }
 //         });
-    
-      }, 2000);
+
+        }, 2000);
       });
     }
     return true;
@@ -58,24 +57,28 @@ define(['jquery', 'lib/components/base/modal'], function ($, Modal) {
     var modal = Contact.modal(data);
     //setTimeout(modal.destroy.bind(modal), 5000);
     //}
+
+    //self.system().domain
+
   };
   Contact.modal = function (data) {
     var modal = new Modal({
       class_name: 'modal-window',
       init: function ($modal_body) {
-        
+
         $modal_body.css({
           width: '50%',
           height: '100%',
-          overflow : 'auto',
-          padding: '0',
-          });
-        
+          overflow: 'auto',
+          padding: '0'
+        });
+
         var $this = $(this);
         $modal_body
-          .trigger('modal:loaded') //запускает отображение модального окна
-          .html("<iframe src='https://amobase.amocrm.ru/search?q=' + value + '&page=1' + '?compact=yes' class='_support-frame'></iframe>")
-          .trigger('modal:centrify')  //настраивает модальное окно
+          .trigger('modal:loaded')
+          //.html("<iframe src='https://amobase.amocrm.ru/search?q=' + value + '&page=1' + '?compact=yes' class='_support-frame'></iframe>")
+          .html("<iframe src='https://amobase.amocrm.ru/search?q=" + value + "&page=1" + "?compact=yes' class='_amobase-frame'></iframe>")
+          .trigger('modal:centrify')
           .append('<span class="modal-body__close"><span class="icon icon-modal-close"></span></span>');
       },
       destroy: function () {
@@ -83,12 +86,12 @@ define(['jquery', 'lib/components/base/modal'], function ($, Modal) {
     });
     return modal;
   };
-  
-  Contact.getLastChat()
+
+  Contact.getLastChat = function (self)
   {
     //var chats = AMOCRM.constant('amojo_chats')
     return $('.feed-note-wrapper-amojo').last().find('p').text();
-  }
+  };
 
   Contact.init = function (self) {
     console.log('init');
